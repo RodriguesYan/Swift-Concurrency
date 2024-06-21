593732014e1fdd82d3e1c11564d4da7e54d332c9
Yan Rodrigues <yan.rodrigues@avenue.us>
2024-06-21 13:22:18 -0300

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Yan Rodrigues on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
